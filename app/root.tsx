354458d49039463005842019d5c145d77342228f
Facundo Martin Gordillo <facundomgordillO@gmail.com>
2022-03-19 00:00:19 -0300

    feat: added styling + changed `package.json` name + minor text change

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,15 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "remix";
+import resetStylesUrl from "~/styles/reset.css";
+import globalStylesUrl from "~/styles/global.css";
+import darkStylesUrl from "~/styles/dark.css";
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => ({
@@ -13,6 +17,24 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export let links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: resetStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: darkStylesUrl,
+      media: "(prefers-color-scheme: dark)",
+    },
+  ];
+};
 
 export default function App() {
   return (
